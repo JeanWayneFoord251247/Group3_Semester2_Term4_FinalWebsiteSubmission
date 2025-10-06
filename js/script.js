ee607a0476c5e7c1bacf77fd71d416d76f11fc58
shaneoberholzer <241241@virtualwindow.co.za>
2025-10-06 11:50:35 +0200

    comments
    just removed comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
   const searchIcon = document.querySelector('.index-icons a:last-child');
   
-  // Create input element dynamically
+  
   const input = document.createElement('input');
   input.type = 'text';
   input.placeholder = 'Search...';
   input.classList.add('search-input');
   
-  // Insert input before the search icon
+
   searchIcon.parentNode.insertBefore(input, searchIcon);
   
-  // Toggle expand on click
+ 
   searchIcon.addEventListener('click', (e) => {
     e.preventDefault();
     input.classList.toggle('active');
@@ -21,7 +21,6 @@
     }
   });
 
-  // Collapse when losing focus
   input.addEventListener('blur', () => {
     input.classList.remove('active');
   });
@@ -29,7 +28,7 @@
 
 
 
-  // Toggle password visibility
+  
 document.getElementById('togglePassword').addEventListener('click',()=>{
   const input=document.getElementById('passwordInput');
   const eye=document.getElementById('eyeIcon');
